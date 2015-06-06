4e4573696f6922c937614014d17bffd8f4fdf570
Martchus <martchus@gmx.net>
2015-06-07 00:15:02 +0200

    increased version number

--- gui/initiate.cpp
+++ gui/initiate.cpp
@@ -15,7 +15,7 @@ int runWidgetsGui(int argc, char *argv[])
     QApplication::setOrganizationName(QStringLiteral("Martchus"));
     QApplication::setOrganizationDomain(QStringLiteral("http://martchus.netai.net/"));
     QApplication::setApplicationName(QStringLiteral("Tageditor"));
-    QApplication::setApplicationVersion(QStringLiteral("1.1.4"));
+    QApplication::setApplicationVersion(QStringLiteral("1.1.5"));
     // load translation files
     TranslationFiles::loadQtTranslationFile();
     TranslationFiles::loadApplicationTranslationFile(QStringLiteral("tageditor"));
