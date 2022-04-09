Martchus/tageditor

commit e535e2072066ff06b8b4f43dad72278a8493de36

Fix warnings

gui/attachmentsmodel.cpp
@@ -189,6 +189,7 @@ Qt::ItemFlags AttachmentsModel::flags(const QModelIndex &index) const
         switch (index.column()) {
         case 0:
             flags |= Qt::ItemIsUserCheckable;
+            break;
         case 1:
         case 2:
             flags |= Qt::ItemIsEditable;

cli/mainfeatures.cpp
@@ -506,15 +506,15 @@ void setTagInfo(const SetTagInfoArgs &args)
     static string context("setting tags");
     for (const char *file : args.filesArg.values()) {
         Diagnostics diag;
-        AbortableProgressFeedback progress; // FIXME: actually use the progress object
+        AbortableProgressFeedback parsingProgress; // FIXME: actually use the progress object
         try {
             // parse tags and tracks (tracks are relevent because track meta-data such as language can be changed as well)
             cout << TextAttribute::Bold << "Setting tag information for \"" << file << "\" ..." << Phrases::EndFlush;
             fileInfo.setPath(file);
-            fileInfo.parseContainerFormat(diag, progress);
-            fileInfo.parseTags(diag, progress);
-            fileInfo.parseTracks(diag, progress);
-            fileInfo.parseAttachments(diag, progress);
+            fileInfo.parseContainerFormat(diag, parsingProgress);
+            fileInfo.parseTags(diag, parsingProgress);
+            fileInfo.parseTracks(diag, parsingProgress);
+            fileInfo.parseAttachments(diag, parsingProgress);
             vector<Tag *> tags;
 
             // remove tags with the specified targets
@@ -618,16 +618,16 @@ void setTagInfo(const SetTagInfoArgs &args)
                             // add value from file
                             try {
                                 // assume the file refers to a picture
-                                MediaFileInfo fileInfo(relevantDenotedValue->value);
-                                Diagnostics diag;
-                                AbortableProgressFeedback progress; // FIXME: actually use the progress object
-                                fileInfo.open(true);
-                                fileInfo.parseContainerFormat(diag, progress);
-                                auto buff = make_unique<char[]>(fileInfo.size());
-                                fileInfo.stream().seekg(static_cast<streamoff>(fileInfo.containerOffset()));
-                                fileInfo.stream().read(buff.get(), static_cast<streamoff>(fileInfo.size()));
-                                TagValue value(move(buff), fileInfo.size(), TagDataType::Picture);
-                                value.setMimeType(fileInfo.mimeType());
+                                MediaFileInfo coverFileInfo(relevantDenotedValue->value);
+                                Diagnostics coverDiag;
+                                AbortableProgressFeedback coverProgress; // FIXME: actually use the progress object
+                                coverFileInfo.open(true);
+                                coverFileInfo.parseContainerFormat(coverDiag, coverProgress);
+                                auto buff = make_unique<char[]>(coverFileInfo.size());
+                                coverFileInfo.stream().seekg(static_cast<streamoff>(coverFileInfo.containerOffset()));
+                                coverFileInfo.stream().read(buff.get(), static_cast<streamoff>(coverFileInfo.size()));
+                                TagValue value(move(buff), coverFileInfo.size(), TagDataType::Picture);
+                                value.setMimeType(coverFileInfo.mimeType());
                                 convertedValues.emplace_back(move(value));
                             } catch (const TagParser::Failure &) {
                                 diag.emplace_back(DiagLevel::Critical, "Unable to parse specified cover file.", context);
@@ -703,8 +703,8 @@ void setTagInfo(const SetTagInfoArgs &args)
             bool attachmentsModified = false;
             if (args.addAttachmentArg.isPresent() || args.updateAttachmentArg.isPresent() || args.removeAttachmentArg.isPresent()
                 || args.removeExistingAttachmentsArg.isPresent()) {
-                static const string context("setting attachments");
-                fileInfo.parseAttachments(diag, progress);
+                static const string attachmentsContext("setting attachments");
+                fileInfo.parseAttachments(diag, parsingProgress);
                 if (fileInfo.attachmentsParsingStatus() == ParsingStatus::Ok && container) {
                     // ignore all existing attachments if argument is specified
                     if (args.removeExistingAttachmentsArg.isPresent()) {
@@ -737,8 +737,8 @@ void setTagInfo(const SetTagInfoArgs &args)
                         attachmentsModified |= currentInfo.next(container, diag);
                     }
                 } else {
-                    diag.emplace_back(
-                        DiagLevel::Critical, "Unable to assign attachments because the container object has not been initialized.", context);
+                    diag.emplace_back(DiagLevel::Critical, "Unable to assign attachments because the container object has not been initialized.",
+                        attachmentsContext);
                 }
             }
 
@@ -746,11 +746,11 @@ void setTagInfo(const SetTagInfoArgs &args)
             fileInfo.setSaveFilePath(currentOutputFile != noMoreOutputFiles ? string(*currentOutputFile) : string());
             try {
                 // create handler for progress updates and aborting
-                AbortableProgressFeedback progress(logNextStep, logStepPercentage);
-                const InterruptHandler handler(bind(&AbortableProgressFeedback::tryToAbort, ref(progress)));
+                AbortableProgressFeedback applyProgress(logNextStep, logStepPercentage);
+                const InterruptHandler handler(bind(&AbortableProgressFeedback::tryToAbort, ref(applyProgress)));
 
                 // apply changes
-                fileInfo.applyChanges(diag, progress);
+                fileInfo.applyChanges(diag, applyProgress);
 
                 // notify about completion
                 finalizeLog();
@@ -855,7 +855,7 @@ void extractField(
                                                       : outputFileArg.values().front();
                         try {
                             outputFileStream.open(path, ios_base::out | ios_base::binary);
-                            outputFileStream.write(value.first->dataPointer(), value.first->dataSize());
+                            outputFileStream.write(value.first->dataPointer(), static_cast<std::streamsize>(value.first->dataSize()));
                             outputFileStream.flush();
                             cout << " - Value has been saved to \"" << path << "\"." << endl;
                         } catch (const std::ios_base::failure &) {
@@ -865,7 +865,7 @@ void extractField(
                 } else {
                     // write data to stdout if no output file has been specified
                     for (const auto &value : values) {
-                        cout.write(value.first->dataPointer(), value.first->dataSize());
+                        cout.write(value.first->dataPointer(), static_cast<std::streamsize>(value.first->dataSize()));
                     }
                 }
             } else {

dbquery/dbquery.cpp
@@ -86,7 +86,7 @@ TagValue QueryResultsModel::fieldValue(int row, KnownField knownField) const
     case KnownField::Cover:
         if (!res.cover.isEmpty()) {
             TagValue tagValue(res.cover.data(), static_cast<size_t>(res.cover.size()), TagDataType::Picture);
-            tagValue.setMimeType(containerMimeType(parseSignature(res.cover.data(), res.cover.size())));
+            tagValue.setMimeType(containerMimeType(parseSignature(res.cover.data(), static_cast<std::size_t>(res.cover.size()))));
             return tagValue;
         }
         break;

dbquery/musicbrainz.cpp
@@ -251,14 +251,14 @@ void MusicBrainzResultsModel::parseInitialResults(const QByteArray &data)
     }
     // -> sort recordings within each release by track number and add recordings to results
     for (auto &releaseAndRecordings : recordingsByRelease) {
-        auto &recordings = releaseAndRecordings.second;
-        std::sort(recordings.begin(), recordings.end(), [](const auto &recording1, const auto &recording2) {
+        auto &recordingsOfRelease = releaseAndRecordings.second;
+        std::sort(recordingsOfRelease.begin(), recordingsOfRelease.end(), [](const auto &recording1, const auto &recording2) {
             if (recording1.disk != recording2.disk) {
                 return recording1.disk < recording2.disk;
             }
             return recording1.track < recording2.track;
         });
-        for (auto &recording : recordings) {
+        for (auto &recording : recordingsOfRelease) {
             m_results << move(recording);
         }
     }

gui/attachmentsedit.cpp
@@ -56,7 +56,7 @@ void AttachmentsEdit::setFileInfo(TagParser::MediaFileInfo *fileInfo, bool updat
     if (fileInfo && fileInfo->areAttachmentsSupported()) {
         if (AbstractContainer *container = fileInfo->container()) {
             auto count = container->attachmentCount();
-            m_currentAttachments.reserve(count);
+            m_currentAttachments.reserve(static_cast<decltype(m_currentAttachments)::size_type>(count));
             for (size_t i = 0; i < count; ++i) {
                 m_currentAttachments << container->attachment(i);
             }
@@ -136,7 +136,7 @@ void AttachmentsEdit::extractSelected()
                     NativeFileStream file;
                     file.exceptions(ios_base::badbit | ios_base::failbit);
                     try {
-                        input.seekg(data->startOffset());
+                        input.seekg(static_cast<std::streamoff>(data->startOffset()), std::ios_base::beg);
                         file.open(toNativeFileName(fileName).data(), ios_base::out | ios_base::binary);
                         CopyHelper<0x1000> helper;
                         helper.copy(input, file, data->size());

gui/dbquerywidget.cpp
@@ -508,8 +508,8 @@ void DbQueryWidget::applyResults(TagEdit *tagEdit, const QModelIndex &resultInde
                 if (row != index.row()) {
                     return;
                 }
-                if (auto *const tagEdit = m_tagEditorWidget->activeTagEdit()) {
-                    tagEdit->setValue(KnownField::Cover, m_model->fieldValue(row, KnownField::Cover), previousValueHandling);
+                if (auto *const activeTagEdit = m_tagEditorWidget->activeTagEdit()) {
+                    activeTagEdit->setValue(KnownField::Cover, m_model->fieldValue(row, KnownField::Cover), previousValueHandling);
                 }
             });
             break;
@@ -531,8 +531,8 @@ void DbQueryWidget::applyResults(TagEdit *tagEdit, const QModelIndex &resultInde
                 if (row != index.row()) {
                     return;
                 }
-                if (auto *const tagEdit = m_tagEditorWidget->activeTagEdit()) {
-                    tagEdit->setValue(KnownField::Lyrics, m_model->fieldValue(row, KnownField::Lyrics), previousValueHandling);
+                if (auto *const activeTagEdit = m_tagEditorWidget->activeTagEdit()) {
+                    activeTagEdit->setValue(KnownField::Lyrics, m_model->fieldValue(row, KnownField::Lyrics), previousValueHandling);
                 }
             });
             break;

gui/entertargetdialog.cpp
@@ -9,6 +9,7 @@
 #include <tagparser/mediafileinfo.h>
 #include <tagparser/signature.h>
 
+#include <qtutilities/misc/conversion.h>
 #include <qtutilities/misc/dialogutils.h>
 #include <qtutilities/models/checklistmodel.h>
 
@@ -89,9 +90,8 @@ EnterTargetDialog::~EnterTargetDialog()
 
 void EnterTargetDialog::updateLevelNamePlaceholderText(int i)
 {
-    const auto levelName
-        = i >= 0 ? tagTargetLevelName(containerTargetLevel(m_currentContainerFormat, static_cast<std::uint32_t>(i))) : std::string_view();
-    m_ui->levelNameLineEdit->setPlaceholderText(QString::fromUtf8(levelName.data(), levelName.size()));
+    m_ui->levelNameLineEdit->setPlaceholderText(qstringFromStdStringView(
+        i >= 0 ? tagTargetLevelName(containerTargetLevel(m_currentContainerFormat, static_cast<std::uint32_t>(i))) : std::string_view()));
 }
 
 TagParser::TagTarget EnterTargetDialog::target() const

gui/fileinfomodel.cpp
@@ -51,7 +51,7 @@ QStandardItem *defaultItem(const QString &text)
 
 QStandardItem *defaultItem(std::string_view text)
 {
-    return defaultItem(QString::fromUtf8(text.data(), text.size()));
+    return defaultItem(qstringFromStdStringView(text));
 }
 
 class ItemHelper {
@@ -72,7 +72,7 @@ public:
     void appendRow(const QString &label, std::string_view text)
     {
         if (!text.empty()) {
-            appendRow(label, QString::fromUtf8(text.data(), text.size()));
+            appendRow(label, qstringFromStdStringView(text));
         }
     }
 
@@ -313,10 +313,9 @@ void FileInfoModel::updateCache()
         QString containerName;
         const auto containerFormatName = m_file->containerFormatName();
         if (const auto subversion = m_file->containerFormatSubversion(); !subversion.empty()) {
-            containerName = QString::fromUtf8(containerFormatName.data(), containerFormatName.size()) % QChar(' ')
-                % QString::fromUtf8(subversion.data(), subversion.size());
+            containerName = qstringFromStdStringView(containerFormatName) % QChar(' ') % qstringFromStdStringView(subversion);
         } else {
-            containerName = QString::fromUtf8(containerFormatName.data(), containerFormatName.size());
+            containerName = qstringFromStdStringView(containerFormatName);
         }
         setItem(currentRow, 1, defaultItem(containerName));
 
@@ -465,8 +464,8 @@ void FileInfoModel::updateCache()
                     if (const auto cc = track->channelConfigString(); !cc.empty()) {
                         const auto ecc = track->extensionChannelConfigString();
                         trackHelper.appendRow(tr("Channel config"),
-                            !ecc.empty() ? QString::fromUtf8(ecc.data(), ecc.size()) % QStringLiteral(" / ") % QString::fromUtf8(cc.data(), cc.size())
-                                         : QString::fromUtf8(cc.data(), cc.size()));
+                            !ecc.empty() ? qstringFromStdStringView(ecc) % QStringLiteral(" / ") % qstringFromStdStringView(cc)
+                                         : qstringFromStdStringView(cc));
                     } else {
                         trackHelper.appendRow(tr("Channel count"), track->channelCount());
                     }

commit 4cb728ca486999a9070b9bde9ab589618567f0a3

Restore editability of attachment name in GUI broken by e535e207

gui/attachmentsmodel.cpp
@@ -189,7 +189,7 @@ Qt::ItemFlags AttachmentsModel::flags(const QModelIndex &index) const
         switch (index.column()) {
         case 0:
             flags |= Qt::ItemIsUserCheckable;
-            break;
+            [[fallthrough]];
         case 1:
         case 2:
             flags |= Qt::ItemIsEditable;